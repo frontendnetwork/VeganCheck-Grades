bc651235eeaddcfb3e5c1257201613be781fdcdf
Philip <4144601+philipbrembeck@users.noreply.github.com>
2023-01-18 13:33:20 +0100

    -

---
+++ src/components/Home.js
@@ -0,0 +1,147 @@
+import $ from "jquery"
+import React, { useEffect } from 'react';
+
+export default function Home() {
+    useEffect(() => {
+        $('.retry').on('click', function(e) {
+            e.preventDefault();
+            $('.ratingform').removeClass("hidden");
+            $('.checked').addClass("hidden");
+            $('.submitted').addClass("hidden");
+    
+        });
+    
+        $('button[name="submit"]').on('click', function(e) {
+            let svg = "<svg xmlns=\"http:\/\/www.w3.org\/2000\/svg\" xml:space=\"preserve\" viewBox=\"0 0 100 100\">\r\n  <circle cx=\"6\" cy=\"50\" r=\"6\" class=\"circle\">\r\n    <animate attributeName=\"opacity\" begin=\".1\" dur=\"1s\" repeatCount=\"indefinite\" values=\"0;1;0\"\/>\r\n  <\/circle>\r\n  <circle cx=\"26\" cy=\"50\" r=\"6\" class=\"circle\">\r\n    <animate attributeName=\"opacity\" begin=\".2\" dur=\"1s\" repeatCount=\"indefinite\" values=\"0;1;0\"\/>\r\n  <\/circle>\r\n  <circle cx=\"46\" cy=\"50\" r=\"6\" class=\"circle\">\r\n    <animate attributeName=\"opacity\" begin=\".3\" dur=\"1s\" repeatCount=\"indefinite\" values=\"0;1;0\"\/>\r\n  <\/circle>\r\n<\/svg>";
+            let text = "Request rating";
+            $('button[name="submit"]').html(svg);
+            e.preventDefault();
+    
+            $.ajax({
+                url: 'https://cldsi.de/test/backend.php',
+                type: 'POST',
+                data: {
+                    barcode: $('input[name="barcode"]').val()
+                },
+                error: function() {
+                    $('button[name="submit"]').html(text);
+                    $('.ratingform').addClass("shake");
+                    setTimeout(
+                        function() {
+                            $('.ratingform').removeClass("shake");
+                        }, 600);
+                },
+                success: function(result) {
+                    $('button[name="submit"]').html(text);
+                    if (!result) {
+                        $('.ratingform').addClass("hidden");
+                        $('.submitted').removeClass("hidden");
+                    } else {
+                        $('.productname').html(result.name);
+                        $('.productrating').html(result.grade);
+                        $('.ratingform').addClass("hidden");
+                        $('.checked').removeClass("hidden");
+                    }
+                }
+            });
+        });
+      })
+    return(<>
+    <img src="assets/img/vcg.svg" className="rotate_logo" />
+    <section className="flexbox">
+        <div className="flexbox-left">
+        </div>
+        <div className="flexbox-right">
+            <h1>Get actual useful product ratings. No bullshit.<span className="icon-stars"></span></h1>
+            <h2>Are you confused by all the vegan/eco-friendly/organic labels? </h2>
+            <p>You will be no more with the easy to understand VeganCheck Grades.<br />VeganCheck Grades is a rating system for vegan products, that assigns one of six grades to a product.</p>
+            <div className="centered"><a href="#ourgrades" className="btn">Learn more</a></div>
+        </div>
+    </section>
+    <section id="ourgrades">
+        <h1>Our grades</h1>
+        <p>The products with a VeganCheck Grade are evaluated by a small team of volunteers and categorized according to their characteristics.</p>
+        <div className="hero">
+            <div className="box">
+                <h2>Grade A+</h2>
+                <p>Only the best of the best products get a Grade A+! Not only has the product to be vegan and cruelty-free, but it also can not cotain palm-oil, has to be produced carbon-neutral and with renewable energies, has to have a fair price and much more. A product rated A+ is recommended to buy!</p>
+            </div>
+            <div className="box">
+                <h2>Grade A</h2>
+                <p>Grade A is the second highest grade and everything from Grade A+ applies, except for the fair price, the usage of renewable energies and an indipendent laboratory analysis if it is a supplement or suppelemental product (e.g. protein shakes). A product rated A is recommended to buy! </p>
+            </div>
+            <div className="box">
+                <h2>Grade B</h2>
+                <p>Grade B still has high standards, such as not made with palm-oil, no animal-cruelty, the manufacturer is not associated with any other harmful actions, good nutritional value if applicable (foods), proven health-claims if applicable (food and beauty) and proven effect if applicable (beauty). A product rated B is recommended to buy!</p>
+            </div>
+        </div>
+        <div className="hero">
+            <div className="box">
+                <h2>Grade C</h2>
+                <p>Grade C is still an average score. The product has to be made with at <strong>least</strong> sustainable palm-oil, has to have a good nutritional value if applicable (foods), has to have proven health-claims if applicable (food and beauty) and has to have a proven effect if applicable (beauty). Depending on your standards, you should consider if you want to buy a product rated C. </p>
+            </div>
+            <div className="box">
+                <h2>Grade D</h2>
+                <p>Grade D is the worst grade a product can get, besides "Not eligible". Products rated with D have to have proven health-claims if applicable (food and beauty) and a proven effect if applicable (beauty). You should generelly consider a better rated option to buy.</p>
+            </div>
+            <div className="box">
+                <h2>Not eligible</h2>
+                <p>All other products are not eligible for evaluation and are not recommended for purchase.</p>
+            </div>
+        </div>
+    </section>
+    <section className="grade_sample">
+        <img src="assets/img/productsample.svg" />
+        <div className="image-container">
+            <img src="assets/img/grade/Grade A+.svg" />
+            <img src="assets/img/grade/Grade B.svg" />
+            <img src="assets/img/grade/Grade C.svg" />
+            <img src="assets/img/grade/Grade A.svg" />
+            <img src="assets/img/grade/Grade D.svg" />
+            <img src="assets/img/grade/Grade NE.svg" />
+        </div>
+    </section>
+    <section className="app">
+        <div className="app-flex">
+            <div className="app-flex-left">
+                <h1><span className="icon-stars"></span>Everything at a fingertip</h1>
+                <p>VeganCheck Grades are now available within VeganCheck.me!</p>
+                <p>Gone are the days of spending hours in the grocery store reading labels and trying to decipher ingredient lists. With VeganCheck.me, finding vegan-friendly products has never been easier: Simply scan the barcode of any product with your smartphone, and our user-friendly app will let you know if it's vegan or not.</p>
+                <p><a href="https://vegancheck.me" className="btn">Use VeganCheck.me</a> <a href="https://shareshortcuts.com/download/2224-vegancheck.html" className="btn">Get the iOS Shortcut</a></p>
+            </div>
+            <div className="app-flex-right">
+                <img src="assets/img/phone.svg" />
+            </div>
+        </div>
+    </section>
+    <section className="rating">
+        <div className="rating-left">
+            <h1><span className="icon-heart"></span>Request a product rating</h1>
+            <p>Individual costumers as well as producers are abled to request a VeganCheck Grade for a certain product. Producers can also send additional information (such as a certificate of using renweable energies in production) to us via <a href="mailto:grades@vegancheck.me">e-mail</a>, to increase their (already existing) product rating.</p>
+            <form action="" method="post" className="ratingform centered">
+                <input type="number" name="barcode" placeholder="Enter barcode" />
+                <button name="submit">Request rating</button>
+            </form>
+            <div className="submitted hidden">
+                <div className="icon-tick"></div>
+                <p>Your request was successful.<br /><span className="small">It usually takes 48 hours for a product to be rated, but it can take longer sometimes.</span><br /><span className="small retry">Click here to request another product rating</span></p>
+            </div>
+            <div className="checked hidden">
+                <div className="icon-cross"></div>
+                <p><span className="productname"></span> is already rated: <span className="productrating"></span><br /><span className="small retry">Click here to retry</span></p>
+            </div>
+        </div>
+        <div className="rating-right">
+        </div>
+    </section>
+    <section className="section-timeline">
+        <div className="section-timeline-time">
+            <p><span className="date">March 2022</span><br />The idea of VeganCheck Grades gets proposed on GitHub <span className="icon-star"></span></p>
+            <p><span className="date">March 2022</span><br />First concept of the grades created, added to the roadmap for VeganCheck v2.0</p>
+            <p><span className="date">April 2022</span><br />First volunteers rate products for VeganCheck Grades &amp; first 100 ratings awarded <span className="icon-heart"></span></p>
+            <p><span className="date">February 2023</span><br /><span className="icon-exclaim"></span>Public launch of VeganCheck Grades with VeganCheck v2.2</p>
+        </div>
+    </section>
+    </>
+    );
+}
---
+++ src/components/Legal.js
@@ -0,0 +1,60 @@
+export default function Legal() {
+    return(<>
+    <section className="onepage">
+        <h1>Legal<span className="icon-stars"></span></h1>
+         <p><b>Angaben gemäß § 5 TMG:</b></p>
+ <p><script>document.write(window.location.hostname)</script> ist ein Angebot von:</p>
+    <p>
+        Philip Brembeck<br />
+        c/o philip.media<br />
+        Silcherstraße 5<br />
+        80807 München<br />
+        Deutschland
+    </p>
+    
+    <p><b>Kontakt</b></p>
+        <p>
+            Telefon: <a href="tel:+498912138384">+49 (0) 89 12138384</a><br/>
+            Mobil: <a href="tel:+4915678575537">+49 (0) 156 78575537</a><br/>
+            E-Mail: <a href="mailto:philip@brembeck.me">philip@brembeck.me</a> (<a href="https://cldsi.de/pgpkey.txt">Public PGP-Key herunterladen</a>)
+        </p>
+        <p>Der Security-Kontakt ist in der <a href="https://cldsi.de/.well-known/security.txt">security.txt</a> zu finden.</p>
+
+    <p><b>Umsatzsteuer</b></p>
+    <p>
+    Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz:<br/>
+    DE95146213080
+    </p>
+
+    <p><b>Angaben zur Berufshaftpflichtversicherung</b></p>
+    <p>
+    Name und Sitz des Versicherers:<br/>
+    Gewerkschaft Erziehung und Wissenschaft<br/>
+    Landesverband Bayern, Geschäftsstelle<br/>
+    Neumarkter Straße 22<br/>
+    81673 München
+    </p>
+    <p>
+    Geltungsraum der Versicherung: Deutschlandweit 
+    </p>
+
+    <p><b>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV</b></p>
+    <p>
+    Philip Brembeck<br/>
+    Silcherstraße 5<br/>
+    80807 München
+    </p>
+
+    <p><b>Datenschutz</b></p>
+            <ul>
+                <li>Es werden keinerlei Serverlogs angefertigt und ein Tracking findet nicht statt.</li>
+                <li>Alle Eingaben auf dieser Seite werden (SSL-)verschlüsselt übertragen.</li>
+                <li>Diese Seite liegt auf einem Server von <a href="https://uberspace.de">UberSpace</a> in Deutschland.</li>
+                <li>Die Domain zu dieser Seite liegt bei 1&amp;1 IONOS.</li>
+                <li>Es liegen sowohl bei Uberspace als auch bei 1&amp;1 IONOS Aufträge gemäß Art. 28 DSGVO vor.</li>
+            </ul>
+        
+    </section>
+    </>
+    );
+}
